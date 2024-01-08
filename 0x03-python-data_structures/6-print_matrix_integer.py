89af4bd3d1d25359ec5b169d43c02afd4cdff958
Khaya14 <khayashezi14@gmail.com>
2024-01-08 17:04:12 +0200

    Matrix corr

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for n in range(len(matrix[i])):
             if n != 0:
                 print(" ", end='')
-            print("{:d}".format(matrix[i][j]), end='')
+            print("{:d}".format(matrix[i][n]), end='')
         print()
