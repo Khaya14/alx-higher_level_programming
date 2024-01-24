73a6ad0d154660eb115e4ff9fba7fa40b79ad807
Khaya14 <khayashezi14@gmail.com>
2024-01-24 11:16:21 +0200

    0-square

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-
 """Filename: 0-square"""
 
+
+
 class Square:
-    """An empty class Square that defines a square."""
+    """An empty class Square that defines a square:"""
     pass
